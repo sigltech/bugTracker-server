eba11b852fb35768c80837b251a5b0723fc1bb0d
sigltech <101203107+sigltech@users.noreply.github.com>
2022-10-05 09:18:46 +0100

    this commit removes the teams table as is it seemigly irrelivant if users are split into "projects"

--- bug_tracker_server/models/projects.py
+++ bug_tracker_server/models/projects.py
@@ -6,12 +6,12 @@ class Project(db.Model):
     id = db.Column(db.String(80), primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(120), nullable=False)
-    team = db.Column(db.String(80), db.ForeignKey('teams.name'))
+    # team = db.Column(db.String(80), db.ForeignKey('teams.name'))
     ceated_on = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
 
-    def __init__(self, id, name, description, team, ceated_on):
+    def __init__(self, id, name, description, ceated_on):
         self.id = id
         self.name = name
         self.description = description
-        self.team = team
+        # self.team = team
         self.ceated_on = ceated_on
